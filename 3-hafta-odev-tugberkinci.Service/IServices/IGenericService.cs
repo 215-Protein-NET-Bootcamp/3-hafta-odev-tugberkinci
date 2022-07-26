7d9d08a9356b7be504472da1014ff79351cbc70d
tugberkinci <r.tugberkinci@gmail.com>
2022-07-26 23:38:56 +0300

    tiers has been added

---
+++ 3-hafta-odev-tugberkinci.Service/IServices/IGenericService.cs
@@ -0,0 +1,11 @@
+﻿namespace _3_hafta_odev_tugberkinci.Service.IServices
+{
+    public interface IGenericService <T> where T : class
+    {
+        IEnumerable<T> GetAll ();
+        T GetById (int id);
+        T Insert(T entity);
+        T Update (T entity);
+        T DeleteById (int id);
+    }
+}
